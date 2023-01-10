2e17e2f0f8afd8edf44d7d00cc014c7644f633d4
Rodolphe Peccatte <rodolphe.peccatte@mfa.fr>
2023-01-10 22:17:26 +0100

    remove unnecessary malloc in main()

--- main.c
+++ main.c
@@ -13,14 +13,12 @@ int main()
     Graph* graph = graph_parse_nodes("inputs/alpes_noeuds.csv");
     graph_parse_arcs(graph, "inputs/alpes_arcs.csv");
 
-    Mat *mat = (Mat *)malloc(sizeof(Mat));
-    assert(mat);
-    Mat *pred = (Mat *)malloc(sizeof(Mat));
-    assert(pred);
+    Mat mat;
+    Mat pred;
 
     /* ALGO FLOYD-WARSHALL */
-    init_matrice(graph, mat, pred);
-    floydwarshall(mat, pred);
+    init_matrice(graph, &mat, &pred);
+    floydwarshall(&mat, &pred);
 
     int src = 142;
     int dst = 138;
@@ -29,28 +27,24 @@ int main()
     printf("\nListe des cols et villes des Alpes : \n");
     graph_print_nodes(graph);
 
-    Path *p = (Path *)malloc(sizeof(Path));
-    assert(p);
-    p->size = 0;
-    p->path = NULL;
+    Path p;
+    p.size = 0;
+    p.path = NULL;
 
     // Calcul l'itinéraire du meilleur chemin à partir de la matrice pred
-    itineraryAtoB(pred, src, dst, graph, p);
+    itineraryAtoB(&pred, src, dst, graph, &p);
 
-    if (mat->mat[src][dst] != INF)
+    if (mat.mat[src][dst] != INF)
     {
-        printf("\nLe dénivelé entre %s et %s est de %d m \n", graph->nodes[src].nom, graph->nodes[dst].nom, mat->mat[src][dst]);
-        printf("Le dénivelé cumulé positif de l'itinéraire est de %d m D+ \n", elevation_gain(graph, pred, p));
-        print_itinerary(graph, p, src, dst);
+        printf("\nLe dénivelé entre %s et %s est de %d m \n", graph->nodes[src].nom, graph->nodes[dst].nom, mat.mat[src][dst]);
+        printf("Le dénivelé cumulé positif de l'itinéraire est de %d m D+ \n", elevation_gain(graph, &pred, &p));
+        print_itinerary(graph, &p, src, dst);
     }
     else
     {
         printf("Malheureusement, %s ne permet pas d'atteindre %s selon les données que nous disposons\n", graph->nodes[src].nom, graph->nodes[dst].nom);
     }
 
-    free(mat);
-    free(pred);
-    free(p);
     free(graph);
     return 0;
 }
